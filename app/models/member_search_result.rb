bf38aa68d293d96756c4069666064506e7c9a035
Leah Miller <lkmiller.87@gmail.com>
2019-01-28 10:52:17 -0700

    Adds html to search index

--- app/models/member_search_result.rb
+++ app/models/member_search_result.rb
@@ -1,9 +1,17 @@
+require 'pry'
+
 class MemberSearchResult
   def initialize(state)
-
+    @state = state
   end
 
   def members
-    
+    conn = Faraday.new(:url => "https://api.propublica.org") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV["propublica_key"]
+      faraday.adapter Faraday.default_adapter
+    end
+    response = conn.get("/congress/v1/members/house/#{@state}/current.json")
+    data = JSON.parse(response.body, symbolize_names: true)
+    data[:results]
   end
 end
